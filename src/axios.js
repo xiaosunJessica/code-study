0b4fb1adfcb8ae77e79eac08c43e978dc1d3a294
sunyaqin001 <sunyaqin001@ke.com>
2021-02-23 13:14:17 +0800

    update

--- src/axios.js
+++ src/axios.js
@@ -1,3 +1,17 @@
+const xhr = (config) => {
+  // 解构config, data, 如果不传默认为null, method不传默认为get方法， url是必传参数
+  const { data = null, url, method = 'get' } = config;
+
+  // 实例化XMLHttpRequest
+  const request = new XMLHttpRequest();
+
+  // 初始化一个请求
+  request.open(method.toUpperCase(), url, true);
+
+  // 发送请求
+  request.send(data);
+
+}
 class Axios {
   // 用来存储配置信息。
   config = {};
@@ -7,6 +21,29 @@ class Axios {
   }
   // 该类有一个 request 方法，它可以用来发送请求
   request(config){}
+  get(){}
+  delete(){}
+  head(){}
+  options(){}
+  post(){}
+  put(){}
+  patch(){}
+}
+
+function extend(to, from, ctx) {
+  // 继承方法
+  Object.getOwnPropertyNames(from).forEach((key) => {
+    to[key] = from[key].bind(ctx)
+  })
+
+   // 继承 ctx 自身属性（不继承原型链上属性，因此需要 hasOwnProperty 进行判断）
+  for (let val in ctx) {
+    if (ctx.hasOwnProperty(val)) {
+      to[val] = ctx[val]
+    }
+  }
+
+  return to;
 }
 
 function createInstance(initConfig) {
@@ -14,6 +51,20 @@ function createInstance(initConfig) {
   const context = new Axios(initConfig);
   // 变量instance保存了Axios类上的request方法，并使用上一步实例化的对象去阶梯该方法的this.
   const instance = Axios.prototype.request.bind(context);
+  console.log(instance, context, 'createInstance')
+  extend(instance, Axios.prototype, context)
   // 返回的其实是request方法
   return instance;
 }
+
+const defaults = {
+  method: 'get',
+}
+
+const axios = createInstance(defaults);
+
+axios.create = function(config) {
+  return createInstance(defaults);
+}
+
+export default axios;
---
+++ src/test.js
@@ -0,0 +1,8 @@
+axios({
+  method: "post",
+  url: "https://reqres.in/api/users",
+  data: {
+    "name": "frankshi",
+    "job": "FE"
+  },
+});
